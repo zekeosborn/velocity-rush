9ecd810468f55bf1a856cce3fece4484059770ab
Zeke Osborn <zekeosborn@outlook.com>
2025-03-12 04:16:17 +0000

    feat: protect leaderboard api with session

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -1,13 +1,24 @@
+import authOptions from '@/lib/auth-options';
 import prisma from '@/prisma/client';
+import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
+    // Authorization
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    // Get user data
     const users = await prisma.user.findMany({
       where: { longestRun: { gt: 0 } },
       orderBy: { longestRun: 'desc' },
     });
 
+    // Restructure user data
     const leaderboard = users.map((user, index) => ({
       rank: index + 1,
       walletAddress: user.walletAddress,
