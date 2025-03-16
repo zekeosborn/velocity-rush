d667c25aac9f404191003e32acf15652dd5eb3c3
Zeke Osborn <zekeosborn@outlook.com>
2025-03-16 06:18:53 +0000

    feat(schema.prisma): drop banned column

---
+++ src/prisma/migrations/20250316061535_drop_banned_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `banned` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "banned";
